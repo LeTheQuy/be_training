28887a5e98aca300641a3db6496dfd064f93685a
QUY LE THE <quy@gotitapp.co>
2021-06-14 22:22:27 +0700

    fix: update import

--- code/res/store.py
+++ code/res/store.py
@@ -1,4 +1,4 @@
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
 from code.models.store import StoreModel
@@ -12,7 +12,7 @@ class Store(Resource):
         else:
             return {"message": "store not found"}, 404
 
-    @jwt_required()
+    @jwt_required
     def post(self, name):
         if StoreModel.find_by_name(name) is not None:
             return {"message": f"A Store with name {name} already exits"}, 400
@@ -23,6 +23,7 @@ class Store(Resource):
             return {"message": "An error while inserting store"}, 500
         return store.json(), 201
 
+    @jwt_required
     def delete(self, name):
         store = StoreModel.find_by_name(name)
         if store:
